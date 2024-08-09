6a3942f76dd0f1547d53bca59dc2b8565f2987ee
JiHwan Kim <57897955+doram419@users.noreply.github.com>
2024-08-09 12:52:28 +0900

    bound and unbound

--- python_advanced/point_test.py
+++ python_advanced/point_test.py
@@ -15,6 +15,13 @@ def unbound_instance_method():
     Unbound method:
     클래스 메서드에 인스턴스 참조주소를 전달 우회 접근법
     """
+    p = Point()
+    Point.setX(p, 10)    # 인스턴스에 접근하는게 아니라 클래스에 접근하는 것이기에 어디에 접근하는 건지 알지 못해서 명시 필욘
+    Point.setY(p, 20)
+
+    print(p.getX(), p.getY(), sep=",")
+    print(Point.getX(p), Point.getY(p), sep=",")
 
 if __name__ == "__main__":
-    bound_instance_method()
+    # bound_instance_method()
+    unbound_instance_method()
